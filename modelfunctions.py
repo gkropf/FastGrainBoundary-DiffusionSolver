8ae9f35624f717cdbf381c616db9c014b3339f60
G-Siron <53376789+G-Siron@users.noreply.github.com>
2023-10-27 14:29:35 +0200

    Add MCMC for inversion
    Add the Metropolis-Hastings MCMC for inversion of thermal histories

--- modelfunctions.py
+++ modelfunctions.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use("TkAgg")
 import time as systime
 from numpy import *
 from tkinter import ttk
@@ -52,6 +54,7 @@ def forwardmodel_slow(mainapp):
 
         # compute cooling steps
         [rw, cl] = segs.shape;
+        print(rw)
         segtimes = divide(segs[:, 0], dt)
         segtimes = [int(round(x)) for x in segtimes]
         SegDTdt = []
@@ -225,10 +228,9 @@ def forwardmodel_slow(mainapp):
             xresult[:, i] = 1e4 * linspace(dx[i], L[i] - dx[i], xsteps)
 
     #print(systime.time() - start_time)
-    #print(array(xresult).shape)
+    # print(array(xresult).shape)
     #print(array(yresult).shape)
     #print(array(timeresult).shape)
-
 
     return xresult, yresult, timeresult
 
@@ -284,17 +286,17 @@ def calc_single_diffs(xmod, ymod, error_file):
         y_corr=interpolate.splev(xactual,coef)
 
         #return differences divided by sigma
-        diff = divide(y_corr-yactual,uncert)
+        diff = y_corr-yactual
     else:
         diff=[]
 
     return list(diff)
 
 
-def calc_residuals(mainapp,cool_array,reg_alpha):
+def calc_residuals(mainapp,cool_array):
 
     # record all difference from each mineral within each sample
-    D_total=[]
+    Diff_total=[]
     for i in mainapp.page2.forwardmods:
 
         # if cool history has negative temps give high residuals
@@ -304,143 +306,285 @@ def calc_residuals(mainapp,cool_array,reg_alpha):
             ydata = ydata+300
         else:
             xdata, ydata = forwardmodel_fast(mainapp.page2.forwardmodelframe.forwardmodels_var[i].get(), cool_array)
+        Output_x = pandas.DataFrame(xdata)
+        Output_x.to_csv('Output_x' + str(i) + '.csv')
+        Output_y = pandas.DataFrame(ydata)
+        Output_y.to_csv('Output_y' + str(i) + '.csv')
+
 
         for k in range(0,8):
             error_file=mainapp.page2.errorfileframe.errfile_var[(i,k)].get()
             if error_file:
-                D_total=D_total+calc_single_diffs(xdata[:,k],ydata[:,k],error_file)
-
-    residuals=array(D_total).reshape((-1,1))+(.33/25)*(499-min(min(cool_array[:,1]),499))**2
-
-    # compute regularization term for cooling history
-    N = len(residuals)
-    m = len(cool_array)
-
-    #create L matrix to be used as constraint, currently constructed to penalize 2nd derivative
-    L=zeros((m-2,m))
-    for i in range(0,m-2):
-        L[i,i:i+3]=[1, -2, 1]
-
-    #stack to make K vector
-    res=vstack((residuals,matmul(reg_alpha*L,cool_array[:,1]).reshape(-1,1)))
-    return res.reshape(N+m-2)
+                Diff_total = Diff_total + calc_single_diffs(xdata[:,k],ydata[:,k],error_file)
 
 
-def calc_jacob(mainapp,cool_array,curr_res,reg_alpha,past_SSE,past_aLm):
+    # residuals=array(D_total).reshape((-1,1))+(.33/25)*(499-min(min(cool_array[:,1]),499))**2
 
-    # Calculate current sse and smoothness
-    m=len(cool_array)
-    curr_res=calc_residuals(mainapp,cool_array,reg_alpha)
-    curr_SSE=sum(curr_res[0:-(m-2)]**2)
-    curr_aLm=sum(curr_res[-(m-2):]**2)
-    past_SSE.append(curr_SSE)
-    past_aLm.append(curr_aLm)
+    # # compute regularization term for cooling history
+    # N = len(residuals)
+    # m = len(cool_array)
 
-    # Update progress figures
-    mainapp.page2.progwind.line11.set_data(cool_array[:,0],cool_array[:,1])
-    mainapp.page2.progwind.line12.set_data(cool_array[:,0],cool_array[:,1])
-    limx1, limx2 = min(cool_array[:,0]), max(cool_array[:,0])
-    limy1, limy2 = min(cool_array[:,1]), max(cool_array[:,1])
+    # #create L matrix to be used as constraint, currently constructed to penalize 2nd derivative
+    # L=zeros((m-2,m))
+    # for i in range(0,m-2):
+    #     L[i,i:i+3]=[1, -2, 1]
 
-    mainapp.page2.progwind.ax1.set_ylim([limy1-.03*limy2, 1.03*limy2])
-    mainapp.page2.progwind.ax1.set_xlim([limx1-.03*limx2, 1.03*limx2])
+    # #stack to make K vector
+    # res=vstack((residuals,matmul(reg_alpha*L,cool_array[:,1]).reshape(-1,1)))
+    return Diff_total
 
-    # Update 30 most recent objective values into bar plot. The bar graph does not have a built in function
-    # to update the number of entries.
-    num_iter=len(past_SSE)
-    if num_iter<31:
-        for i in range(len(past_SSE)):
-            mainapp.page2.progwind.bar2[i].set_height(past_SSE[i]+past_aLm[i])
-            mainapp.page2.progwind.bar2[i].set_height(past_SSE[i])
+
+# def calc_jacob(mainapp,cool_array,curr_res,reg_alpha,past_SSE,past_aLm):
+
+#     # Calculate current sse and smoothness
+#     m=len(cool_array)
+#     curr_res=calc_residuals(mainapp,cool_array,reg_alpha)
+#     curr_SSE=sum(curr_res[0:-(m-2)]**2)
+#     curr_aLm=sum(curr_res[-(m-2):]**2)
+#     past_SSE.append(curr_SSE)
+#     past_aLm.append(curr_aLm)
+
+#     # Update progress figures
+#     mainapp.page2.progwind.line11.set_data(cool_array[:,0],cool_array[:,1])
+#     mainapp.page2.progwind.line12.set_data(cool_array[:,0],cool_array[:,1])
+#     limx1, limx2 = min(cool_array[:,0]), max(cool_array[:,0])
+#     limy1, limy2 = min(cool_array[:,1]), max(cool_array[:,1])
+
+#     mainapp.page2.progwind.ax1.set_ylim([limy1-.03*limy2, 1.03*limy2])
+#     mainapp.page2.progwind.ax1.set_xlim([limx1-.03*limx2, 1.03*limx2])
+
+#     # Update 30 most recent objective values into bar plot. The bar graph does not have a built in function
+#     # to update the number of entries.
+#     num_iter=len(past_SSE)
+#     if num_iter<31:
+#         for i in range(len(past_SSE)):
+#             mainapp.page2.progwind.bar2[i].set_height(past_SSE[i]+past_aLm[i])
+#             mainapp.page2.progwind.bar2[i].set_height(past_SSE[i])
+#     else:
+#         for i in range(num_iter-30,num_iter):
+#             k = i+(num_iter-30)
+#             mainapp.page2.progwind.bar2[i].set_height(past_SSE[k]+past_aLm[k])
+#             mainapp.page2.progwind.bar2[i].set_height(past_SSE[k])
+
+
+#     mainapp.page2.progwind.canvas1.draw()
+#     mainapp.page2.progwind.canvas2.draw()
+#     mainapp.update()
+
+#     N=len(curr_res)
+#     deltac=1;
+
+#     J=zeros([N,m-2])
+#     for i in range(1,m-1):
+#         mainapp.page2.progwind.calc_var.set("calculating Jacobian column "+str(i)+" of "+str(m-2))
+#         mainapp.update()
+#         new_cool=cool_array.copy()
+#         new_cool[i,1]=new_cool[i,1]+deltac
+#         deriv=(calc_residuals(mainapp,new_cool,reg_alpha)-curr_res)/deltac
+#         J[:,i-1]=deriv.reshape(N)
+
+#     return J
+
+def check_rate(cool_new, params):
+    """ Check that the rates of heating or cooling isn't higher that 100 degC/Ma """
+
+    # Get the time between each point
+    dt = (cool_new[-1,0] - cool_new[0,0]) / len(cool_new)
+
+    # Reads params
+    i = params['iteration']
+    u = params['u']
+    sigma = params['sigma']
+
+    # Check the rate by comparing the selected point 
+    point_i = cool_new[i,1] + u * sigma
+    point_ip = cool_new[i+1,1]
+    point_im = cool_new[i-1,1]
+
+    if sqrt(((point_im - point_i) / dt)**2) >= 100 or sqrt(((point_i - point_ip) / dt)**2) >= 100:
+        print('Looks like the cooling was too high')
+        return True
     else:
-        for i in range(num_iter-30,num_iter):
-            k = i+(num_iter-30)
-            mainapp.page2.progwind.bar2[i].set_height(past_SSE[k]+past_aLm[k])
-            mainapp.page2.progwind.bar2[i].set_height(past_SSE[k])
+        print('Looks like the cooling was fine')
+        return False
+
+def transition_model(cool_array, mainapp, iteration):
+    ### The tranistion model defines how to move temperature points for the ttermal history###
+    move_selection = 1
+
+    # move_selection = random.randint(1,3)  # Randomly select the modification of the cooling history
+    if move_selection == 1:
+    # Move temperature for a point
+        m=len(cool_array)
+        sigma = random.normal(0,1)
+        i = random.randint(1,m-1)
+        cool_new=cool_array.copy()
+        u = 10
+        params = {'iteration' : i,'u' : u, 'sigma' : sigma}
+        cool_new[i,1]=cool_new[i,1] + u * sigma
+
+    elif move_selection == 2:
+    # Add a T-t point
+        cool_new[i,1]=cool_new[i,1] + u * sigma
+
+    elif move_selection ==3:
+    # Remove a T-t point
+        cool_new[i,1]=cool_new[i,1] + u * sigma
 
 
-    mainapp.page2.progwind.canvas1.draw()
-    mainapp.page2.progwind.canvas2.draw()
-    mainapp.update()
+    return cool_new, params
 
-    N=len(curr_res)
-    deltac=1;
 
-    J=zeros([N,m-2])
-    for i in range(1,m-1):
-        mainapp.page2.progwind.calc_var.set("calculating Jacobian column "+str(i)+" of "+str(m-2))
-        mainapp.update()
-        new_cool=cool_array.copy()
-        new_cool[i,1]=new_cool[i,1]+deltac
-        deriv=(calc_residuals(mainapp,new_cool,reg_alpha)-curr_res)/deltac
-        J[:,i-1]=deriv.reshape(N)
+def prior(cool):
+    ### Computes the prior ratio
+    dT = []
+    for i in range(len(cool)-1):
+        dT.append((cool[i+1,1] - cool[i,1])**2)
+    # prior_move_T = 1 / (max(cool[:,1]) - min(cool[:,1]))
+    prior_move_T = 1 / (sum(dT))
+    print(prior_move_T)
 
-    return J
+    return prior_move_T
 
+
+def manual_log_like_normal(diff, error):
+    ###Computes the likelihood of the data given a sigma (new or current) according to equation (2)
+    LL = []
+    pyi = 3.141592653589793
+    for i in range(len(diff)):
+        LL.append(-log(error[i] * sqrt(2 * pyi))-0.5*(((diff[i]))**2) / ((error[i]**2)))
+
+    return sum(LL)
+
+
+#Defines whether to accept or reject the new sample
+def acceptance(cool_array, new_cool):
+    if new_cool>cool_array:
+        return True
+    else:
+        # return False
+        accept=random.uniform(0,1)
+        if accept >= 0.25:
+            return False
+        else:
+            return True
+
+
+def reduced_chi2(diff, error, cool_array):
+    """ Computes the MSWD or reduced chi2 at each iteration using 
+    the difference between measured andmodelled values and the error on the measured value"""
+    chi2 = []
+    for i in range(len(diff)):
+        chi2.append((diff[i] / error[i]) ** 2)
+    MSWD = sum(chi2)/(len(diff) - len(cool_array))
+
+    return MSWD
+
+
+def error_function(error_file):
+
+    for k in range(0,8):
+        error_file=mainapp.page2.errorfileframe.errfile_var[(i,k)].get()
+        if error_file:
+            Uncert = Uncert + calc_single_diffs(xdata[:,k],ydata[:,k],error_file)
+
+        # compute traverse for file
+        def octrav(error_file):
+            replacedat = pandas.read_csv(error_file, sep=' ', header=None).values
+
+            return replacedat[:,0], replacedat[:,1], replacedat[:,2]
+
+        xactual, yactual, uncert = octrav(error_file)
+
+        # keep only right hand side, and where values are in range of model change to mean for actual
+        ind_keep = [i for i in range(len(xactual)) if xactual[i] > 0 and xactual[i] < xmod[-1]]
+        xactual = xactual[ind_keep]
+        yactual = yactual[ind_keep]
+        uncert = uncert[ind_keep]
+
+        return uncert
+
+
+def metropolis_hastings(likelihood_computer,prior, transition_model, init_Tt, iterations, mainapp, acceptance_rule):
+    # likelihood_computer(residuals,uncertainties): returns the likelihood that this thermal history produced the oxygenisotope zoning
+    # transition_model(x): a function that draws a sample from a symmetric distribution and returns it
+    # iterations: number of accepted to be generated
+    # acceptance_rule(cool,cool_new): decides whether to accept or reject the new thermal history
+    cool_array = init_Tt
+    diff = calc_residuals(mainapp, cool_array)
+    uncert = 0.14 * ones((len(diff), 1))
+    cool_accepted = []
+    cool_rejected = []
+    MSWD_accepted = []
+    MSWD_rejected = []
+    log_like = []
+    prior_list = []
+    i_accepted = []
+    i_rejected = []
+    cool_post_burnin = []
+    for i in range(iterations):
+        cool_new, params =  transition_model(cool_array, mainapp, i)
+        cool_post_burnin.append(cool_new)
+        new_diff = calc_residuals(mainapp, cool_new)
+        MSWD = reduced_chi2(diff, uncert, cool_new)
+        cool_lik = likelihood_computer(diff, uncert)
+        log_like.append(cool_lik)
+        prior_list.append(prior(cool_array))
+        cool_new_lik = likelihood_computer(new_diff, uncert)
+        if (acceptance_rule(cool_lik + log(prior(cool_array)),cool_new_lik + log(prior(cool_new)))):        
+            cool_array = cool_new
+            diff = new_diff
+            cool_accepted.append(cool_new)
+            MSWD_accepted.append(MSWD)
+            i_accepted.append(i)
+        else:
+            cool_rejected.append(cool_array)
+            MSWD_rejected.append(MSWD)
+            i_rejected.append(i)   
+                
+    return array(cool_accepted), array(cool_rejected), array(MSWD_accepted), array(MSWD_rejected), i_accepted, i_rejected, array(cool_post_burnin), log_like, prior_list
 
 # This function runs the inverse solver for the minerals, initial profiles given
 def find_inverses(mainapp):
-
-    alpha=0.04032227
 
     for i in range(0,mainapp.page2.num_initials):
         if (mainapp.page2.initsolutions.initial_vars[i].get()==1):
             file_loc=mainapp.page2.initsolutions.folder_var.get()+mainapp.page2.initsolutions.initial_labs[i].get()
             initial_sol=pandas.read_csv(file_loc, sep=',', header=None).values
-            m=len(initial_sol)
+
+    nb_iterations = 20000
+    cool_accepted, cool_rejected, MSWD_accepted, MSWD_rejected, i_accepted, i_rejected, cool_post_burnin, log_likelihood, prior_list = metropolis_hastings(manual_log_like_normal,prior,transition_model,initial_sol,nb_iterations,mainapp,acceptance)
 
 
-            # Calculate initial residuals and smoothness, and create progress plots that will be updated
-            # each time the Jac function is called.
-            initial_res=calc_residuals(mainapp,initial_sol,alpha)
-            initial_SSE=sum(initial_res[0:-(m-2)]**2)
-            initial_aLm=sum(initial_res[-(m-2):]**2)
-
-            past_sse=[initial_SSE]
-            past_aLm=[initial_aLm]
-            mainapp.page2.create_progwind(mainapp,initial_sol,initial_SSE,initial_aLm)
-            mainapp.page2.progwind.init_var.set("Running LM algorithm with starting solution: /"+
-                                                mainapp.page2.initsolutions.initial_labs[i].get())
-
-
-
-
-            # Create residual and jacobian function that accepts single array as input (for built in optimize function)
-            def usey_residuals(cool_y):
-                full_soln=initial_sol.copy()
-                full_soln[1:-1,1]=cool_y
-                return calc_residuals(mainapp, full_soln, alpha)
-
-            def usey_jacob(soln_y):
-                full_soln=initial_sol.copy()
-                full_soln[1:-1,1]=soln_y
-                curr_res=calc_residuals(mainapp, full_soln, alpha)
-                return calc_jacob(mainapp, full_soln, curr_res, alpha, past_sse, past_aLm)
-
-            # Now find solution and convert it back into 2D array
-            initial_solny=initial_sol[1:-1,1].copy()
-            sol=optimize.least_squares(usey_residuals, initial_solny , jac=usey_jacob, method='lm', gtol=1e-9)
-            final_sol=initial_sol.copy()
-            final_sol[1:-1,1]=sol.x
-
-            # Save final solution to Outputs folder
-            final_df = pandas.DataFrame(final_sol)
-            final_df.columns = ['Time', 'Temperature']
-            save_location = 'Output/'+mainapp.page2.initsolutions.initial_labs[i].get()[:-4]+'_results_in_this_sol.csv'
-            final_df.to_csv(save_location, header=True, index=None)
-            
-            # Calculate objective values and SSE
-            final_res=calc_residuals(mainapp,final_sol,alpha)
-            final_SSE=sum(final_res[0:-(m-2)]**2)
-            final_aLm=sum(final_res[-(m-2):]**2)
-
-            # Get magnitude of final gradient function
-            K=calc_jacob(mainapp,final_sol,calc_residuals(mainapp,final_sol,alpha),alpha,past_sse,past_aLm)
-            Kmag=linalg.norm(matmul(transpose(K),final_res))
-            mainapp.page2.progwind.destroy()
-
-
-
-
+    fig = plt.figure(figsize=(15,8))
+    ax1 = fig.add_subplot(1,2,1)
+    ax2 = fig.add_subplot(1,2,2)
+    for i in range(len(cool_post_burnin)):
+        t_post = []
+        T_post = []
+        for j in range(len(cool_post_burnin[i])):
+            t_post.append(cool_post_burnin[i][j,0])
+            T_post.append(cool_post_burnin[i][j,1])
+        ax1.plot(t_post,T_post, 'k--', label='accepted')
+        if i == 0:
+            Data_post = pandas.DataFrame({'Time' : t_post, 'T_cool0' : T_post})
+        else:
+            Data_post = Data_post.assign(T_cool=T_post)
+            Data_post = Data_post.rename(columns={'T_cool' : 'T_cool' + str(i+1)})
+    Data_post.to_csv('Data_post_Burn-in.csv', index=False)
+    Data_log_LL = pandas.DataFrame({'Iteration' : range(nb_iterations), 'Log_LL' : log_likelihood, 'Prior' : prior_list})
+    Data_log_LL.to_csv('Log_Likelihood.csv', index=False)
+    ax2.scatter(range(nb_iterations), log_likelihood)
+    ax2.set_xlabel('Iteration #')
+    ax2.set_ylabel('Log likelihood')
+    init_t = []
+    init_T = []
+    for j in range(len(initial_sol)):
+        init_t.append(initial_sol[j,0])
+        init_T.append(initial_sol[j,1])
+    ax1.plot(init_t,init_T, 'b--', label='initial')
+    ax1.set_ylabel("Temperature (˚C)")
+    ax1.set_xlabel("Time (Ma)")
+    plt.show()
 
 
